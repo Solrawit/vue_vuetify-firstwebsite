e8be58316c7c147a84d283a7d885afe1d8219c16
snoluv45 <pasitza115@gmail.com>
2024-08-11 19:27:46 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template lang="">
   <!-- Banner -->
   <v-img
-  :width="1500"
+  :width="2000"
   aspect-ratio="16/9"
   cover
   src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
-  <center><h1 style="font-size:300%;margin-top:250px;">SUPERFLY PROJECT</h1></center>
+  <center><h1 style="font-size:300%;margin-top:300px;">SUPERFLY PROJECT</h1></center>
   <center><h3 style="font-size:200%;">Deverlopment Vue Vuetify Framework.</h3></center>
   </v-img>
 <!-- ส่วนเนื้อหา -->
